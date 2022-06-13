4b1a8fc0db7a7529621b6dba3427dae7503c013d
rodolphe37 <rodolphe.a@gmail.com>
2022-06-13 14:55:54 +0200

    added first element of the module

--- src/utils/useDebugMode.js
+++ src/utils/useDebugMode.js
@@ -11,55 +11,53 @@ const useDebugMode = ({ BASE_URL, siteName }) => {
   const { UserWithId } = useControlUserId();
 
   const debugConsoles = useCallback(() => {
-    if (geoData !== null && datas.length > 0 && UserWithId !== "") {
-      console.group("PROVIDER = DEBUG_MODE ACTIF");
-      console.group(
-        "PROVIDER = %cuser geolocation",
-        "color: white;  font-weight:bold; background-color: blue;padding: 2px"
-      );
-      console.table([geoData?.ip]);
-      console.groupEnd();
-      console.group(
-        "PROVIDER = %cdata from server",
-        "color: white;  font-weight:bold; background-color:blue;padding: 2px"
-      );
-      console.log(datas);
-      console.groupEnd();
-      console.group(
-        "PROVIDER = %cUserWithId",
-        "color: white;  font-weight:bold; background-color:blue;padding: 2px"
-      );
-      console.log(
-        `%c${UserWithId}`,
-        "font-weight: bold; color:#871F78; padding: 2px"
-      );
-      console.groupEnd();
-      console.group(
-        "PROVIDER = %csite name",
-        "color: white;  font-weight:bold; background-color:blue;padding: 2px"
-      );
-      console.log(
-        `%c${siteName}`,
-        "font-weight: bold;color:#871F78;  padding: 2px"
-      );
-      console.groupEnd();
-      console.group(
-        "PROVIDER = %csessionNumbers",
-        "color: white;  font-weight:bold; background-color:blue;padding: 2px"
-      );
-      console.log(
-        `%c${userSessionObject?.length}`,
-        "font-weight: bold;color:#871F78;  padding: 2px"
-      );
-      console.groupEnd();
-      console.group(
-        "PROVIDER = %cuserSessionObject",
-        "color: white;  font-weight:bold; background-color:blue;padding: 2px"
-      );
-      console.log(userSessionObject);
-      console.groupEnd();
-      console.groupEnd();
-    }
+    console.group("PROVIDER = DEBUG_MODE ACTIF");
+    console.group(
+      "PROVIDER = %cuser geolocation",
+      "color: white;  font-weight:bold; background-color: blue;padding: 2px"
+    );
+    console.table([geoData?.ip]);
+    console.groupEnd();
+    console.group(
+      "PROVIDER = %cdata from server",
+      "color: white;  font-weight:bold; background-color:blue;padding: 2px"
+    );
+    console.log(datas);
+    console.groupEnd();
+    console.group(
+      "PROVIDER = %cUserWithId",
+      "color: white;  font-weight:bold; background-color:blue;padding: 2px"
+    );
+    console.log(
+      `%c${UserWithId}`,
+      "font-weight: bold; color:#871F78; padding: 2px"
+    );
+    console.groupEnd();
+    console.group(
+      "PROVIDER = %csite name",
+      "color: white;  font-weight:bold; background-color:blue;padding: 2px"
+    );
+    console.log(
+      `%c${siteName}`,
+      "font-weight: bold;color:#871F78;  padding: 2px"
+    );
+    console.groupEnd();
+    console.group(
+      "PROVIDER = %csessionNumbers",
+      "color: white;  font-weight:bold; background-color:blue;padding: 2px"
+    );
+    console.log(
+      `%c${userSessionObject?.length}`,
+      "font-weight: bold;color:#871F78;  padding: 2px"
+    );
+    console.groupEnd();
+    console.group(
+      "PROVIDER = %cuserSessionObject",
+      "color: white;  font-weight:bold; background-color:blue;padding: 2px"
+    );
+    console.log(userSessionObject);
+    console.groupEnd();
+    console.groupEnd();
   }, [geoData, datas, UserWithId, siteName, userSessionObject]);
   return {
     debugConsoles,
