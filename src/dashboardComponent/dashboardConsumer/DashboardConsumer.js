ab2c80dc8f592c1db2f83056e8fea331cbfe496b
rodolphe37 <rodolphe.a@gmail.com>
2022-06-29 16:41:43 +0200

    added pages view & count for each page

--- src/dashboardComponent/dashboardConsumer/DashboardConsumer.js
+++ src/dashboardComponent/dashboardConsumer/DashboardConsumer.js
@@ -8,6 +8,7 @@ const DashboardConsumer = ({ children, DEBUG_MODE, BASE_URL }) => {
   const [datas] = useRecoilState(datasAtom);
   const { pathDatas, getPathData } = useGetPathData({ BASE_URL });
   const [pagesViewCounted, setPagesViewCounted] = useState({});
+  const [finalArray, setFinalArray] = useState([]);
 
   useEffectOnce(() => {
     if (pathDatas === null) {
@@ -30,15 +31,44 @@ const DashboardConsumer = ({ children, DEBUG_MODE, BASE_URL }) => {
       }
     }, []);
 
-  useEffect(() => {
+  function find_duplicate_in_array(array) {
     const count = {};
-    pathDatas
-      .map((res) => res.pagesView)
-      .forEach((element, id) => {
-        count[element] = (count[element] || 0) + 1;
-      });
+    const result = [];
 
-    setPagesViewCounted(count);
+    array.forEach((item) => {
+      if (count[item]) {
+        count[item] += 1;
+        return;
+      }
+      count[item] = 1;
+    });
+
+    for (let prop in count) {
+      if (count[prop] >= 2) {
+        result.push([{ page: prop, count: count[prop] }]);
+      }
+    }
+
+    const merged = [].concat.apply([], result);
+    console.log("count", merged);
+    setFinalArray(merged);
+    return merged;
+  }
+
+  useEffect(() => {
+    // const count = {};
+    // pathDatas
+    //   .map((res) => res.pagesView)
+    //   .forEach((element, id) => {
+    //     count[element] = (count[element] || 0) + 1;
+    //   });
+    console.log(
+      "test count ",
+      find_duplicate_in_array(pathDatas.map((res) => res.pagesView))
+    );
+    setPagesViewCounted(
+      find_duplicate_in_array(pathDatas.map((res) => res.pagesView))
+    );
   }, [pathDatas]);
 
   useEffect(() => {
@@ -87,9 +117,29 @@ const DashboardConsumer = ({ children, DEBUG_MODE, BASE_URL }) => {
             <span style={{ color: "#fff" }}>
               <h4 style={{ color: "#61dafb" }}>path with global count</h4>
 
-              <ul>
-                {Object.entries(pagesViewCounted).map((resu, value) => (
-                  <li key={value}>{JSON.stringify(resu)}</li>
+              <ul
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                  listStyle: "none",
+                  lineHeight: 2,
+                  textAlign: "left",
+                }}
+              >
+                {finalArray.map((result, index) => (
+                  <li
+                    key={index}
+                    style={{ border: "1px dotted white", padding: 4 }}
+                  >
+                    Page:{" "}
+                    <span style={{ color: "#61dafb" }}>{result.page}</span>
+                    <br />
+                    Count:{" "}
+                    <span style={{ color: "#61dafb" }}>{result.count}</span>
+                  </li>
                 ))}
               </ul>
             </span>
